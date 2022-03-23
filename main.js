799392309bc831cd03186900913a308a14d2ce0e
Pablo Gutierrez <pablo.gutierrez.munoz@gmail.com>
2022-03-23 16:20:54 +0100

    callbacks, promises, sync_await

--- main.js
+++ main.js
@@ -1,82 +1,107 @@
-/* //notacion literal
+/* const posts = [
+    {
+        "userId": 1,
+        "id": 1,
+        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+        "userId": 1,
+        "id": 2,
+        "title": "qui est esse",
+        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+        "userId": 1,
+        "id": 3,
+        "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    },
+]; */
 
-const regExpLiteral = /helloworld/i;
 
-//notacion de objeto
+// Callbacks
 
-const regExpObjeto = new RegExp("helloworld", "i");
+/* const findPostById = (id, callback) => {
+    
+    const post = posts.find(item => item.id === id);
 
-//Con el test hacemos una comprobacion de que el string que buscamos se encuentra dentro de la variable que le digamos. Devuelve un boolean.
+    if (post) {
+        callback(null, post) //El null hace referencia al string del error de abajo.
+    } else {
+        //respuesta en el error.
+        callback("no se encontro el post con id " + id);
+    }
 
-console.log(regExpObjeto.test("helloworld")); */
-
-//Declaramos variables y hacemos el proces con querySelector
-/* const formulario = document.querySelector("#formulario");
-const userName = document.querySelector("input[name='userName']");
-const userEmail = document.querySelector("input[name='userEmail']"); */
-
-const formulario = document.getElementById("formulario")
-const userName = document.getElementById("userName")
-const userEmail = document.getElementById("userEmail")
-
-const alertName = document.getElementById("alertName")
-const alertEmail = document.getElementById("alertEmail")
-const alertSuccess = document.getElementById("alertSuccess")
-
-//Regular Expressions para la validacion.
-const regUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-const regUserEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
-
-const mostrarMensajeSuccess = () => {
-    alertSuccess.classList.remove("d-none");
-    alertSuccess.textContent = "Formulario enviado con exito.";
-}
-
-const mostrarMensajeError = (erroresValidacion) => {
-    erroresValidacion.forEach((item) => {
-        item.tipo.classList.remove("d-none");
-        item.tipo.textContent = item.msg;
-    });
 };
 
-formulario.addEventListener("submit", (e) => {
-    e.preventDefault(); //para parar el comportamiento por default que hace el navegador al pulsar el boton y asi evitamos que salgan los datos en la uri del navegador.
-
-    const erroresValidacion = [];
-
-    //esto devuelve true si existen espacios
-    //console.log(!userName.value.trim());
-
-    if(!regUserName.test(userName.value) || !userName.value.trim()) {
-        userName.classList.add("is-invalid");
-        erroresValidacion.push({
-            tipo: alertName,
-            msg: "Formato no valido en el campo. Introduce solo letras."
-        });
-    } else {
-        userName.classList.remove("is-invalid");
-        userName.classList.add("is-valid");
-        alertName.classList.add("d-none");
-    };
-
-    if(!regUserEmail.test(userEmail.value) || !userEmail.value.trim()) {
-        userEmail.classList.add("is-invalid");
-        erroresValidacion.push({
-            tipo: alertEmail,
-            msg: "Introduzca un email valido."
-        });
-    } else {
-        userEmail.classList.remove("is-invalid");
-        userEmail.classList.add("is-valid");
-        alertEmail.classList.add("d-none");
+findPostById(4, (err, post) => {
+    if (err) {
+        return console.log(err);
     }
+    console.log(post);
+}); */
 
-    if(erroresValidacion.length !== 0) {
-        mostrarMensajeError(erroresValidacion);
-        return;
+
+//Promises (Otra forma de hacerlo)
+
+/* const findPostById = (id) => {
+
+    const post = posts.find(item => item.id === id);
+
+    return new Promise ((resolve, reject) => {
+
+        if (post) {
+            resolve (post);
+        } else {
+            reject ("no se encontro el post con id " + id);
+        }
+    })
+} */
+
+/* findPostById(4)
+    .then((post) => console.log(post))
+    .catch (err => console.log(err)); */
+
+// Promises con Async / Await (solo en promises)
+
+/* const buscarPost = async (id) => {
+
+    try {
+        const post = await findPostById(id);
+        console.log(post);
+    } catch (err) {
+        console.log(err);
+    } finally {
+        console.log("este console se ejecuta si o si");
     }
+};
 
-    console.log("formulario enviado");
+buscarPost (1); */
 
-    mostrarMensajeSuccess();
-});
+// Fetch
+ 
+const url = 'https://jsonplaceholder.typicode.com/posts/'
+
+/* fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch(err => console.log(err))
+    .finally(() => console.log("finalizar")); */
+
+const findPostById = async (id) => {
+
+    try {
+        const response = await fetch(url + id);
+        const post = await response.json();
+
+        console.log(post);
+
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+findPostById(10);
+
+
